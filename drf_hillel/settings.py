41b18c08621342d6d89c145e8c8db1b7a8e07786
Mankret <asilvergrave@gmail.com>
2023-03-20 14:45:37 +0200

    Hotfix

--- drf_hillel/settings.py
+++ drf_hillel/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d&kzj0+9)%et#_#o)4og9))pime@agdk^34g4(0^#b7tj=^t!n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
